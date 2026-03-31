b1045a18e5b367de9cffcbec1b0cfe916855f79e
dipu-ray <dipu34786@gmail.com>
2026-04-01 00:51:49 +0600

    To add js file and codes

---
+++ Class-22/script.js
@@ -0,0 +1,23 @@
+// Single-line comments
+/* Multi-line comments */
+
+
+document.getElementById("heading2").innerHTML = "<h2>Hello World</h2>"; // innerHTML
+document.getElementById("text").innerText = "Hello World"; // innerText
+document.write(5 + 6); // Document write
+window.alert(5 + 6); // Window alert
+console.log(5 + 6); // Console print
+
+
+// JavaScript statements
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+
+
+// JavaScript code blocks
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+}
